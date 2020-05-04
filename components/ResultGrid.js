62382bd150c26d5031fd811272c6a99162758b21
asger991 <sirjazzper@gmail.com>
2020-05-04 10:51:08 +0200

    updated result page

--- components/ResultGrid.js
+++ components/ResultGrid.js
@@ -10,131 +10,127 @@ const { Meta } = Card;
 const ResultGrid = () => {
   return (
     <div style={containerStyle}>
-      <div style={boxStyle}>
-        <div style={gridStyle}>
-          <Card
-            style={{
-              width: 500,
-              margin: `2rem 2rem`,
-            }}
-            title="Company Name"
-            bordered={false}
-            cover={
-              <img
-                alt="example"
-                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-              />
-            }
-            actions={[
-              <DollarCircleOutlined key="setting" />,
-              <InfoCircleOutlined key="edit" />,
-              <EnvironmentOutlined key="ellipsis" />,
-            ]}
-          >
-            <Meta
-              avatar={
-                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-              }
-              title="Photographer Name"
-              description="This is the description"
+      <div style={gridStyle}>
+        {/* TODO: export card to own component */}
+        {/* TODO: expand height of box on scroll  */}
+        <Card
+          style={{
+            width: 500,
+            margin: `2rem 2rem`,
+          }}
+          title="Company Name"
+          bordered={false}
+          cover={
+            <img
+              alt="example"
+              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
             />
-          </Card>
-          <Card
-            style={{
-              width: 500,
-              margin: `2rem 2rem`,
-            }}
-            title="Company Name"
-            bordered={false}
-            cover={
-              <img
-                alt="example"
-                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-              />
+          }
+          actions={[
+            <DollarCircleOutlined key="setting" />,
+            <InfoCircleOutlined key="edit" />,
+            <EnvironmentOutlined key="ellipsis" />,
+          ]}
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
             }
-            actions={[
-              <DollarCircleOutlined key="setting" />,
-              <InfoCircleOutlined key="edit" />,
-              <EnvironmentOutlined key="ellipsis" />,
-            ]}
-          >
-            <Meta
-              avatar={
-                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-              }
-              title="Photographer Name"
-              description="This is the description"
+            title="Photographer Name"
+            description="This is the description"
+          />
+        </Card>
+        <Card
+          style={{
+            width: 500,
+            margin: `2rem 2rem`,
+          }}
+          title="Company Name"
+          bordered={false}
+          cover={
+            <img
+              alt="example"
+              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
             />
-          </Card>
-          <Card
-            style={{
-              width: 500,
-              margin: `2rem 2rem`,
-            }}
-            title="Company Name"
-            bordered={false}
-            cover={
-              <img
-                alt="example"
-                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-              />
+          }
+          actions={[
+            <DollarCircleOutlined key="setting" />,
+            <InfoCircleOutlined key="edit" />,
+            <EnvironmentOutlined key="ellipsis" />,
+          ]}
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
             }
-            actions={[
-              <DollarCircleOutlined key="setting" />,
-              <InfoCircleOutlined key="edit" />,
-              <EnvironmentOutlined key="ellipsis" />,
-            ]}
-          >
-            <Meta
-              avatar={
-                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-              }
-              title="Photographer Name"
-              description="This is the description"
+            title="Photographer Name"
+            description="This is the description"
+          />
+        </Card>
+        <Card
+          style={{
+            width: 500,
+            margin: `2rem 2rem`,
+          }}
+          title="Company Name"
+          bordered={false}
+          cover={
+            <img
+              alt="example"
+              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
             />
-          </Card>
-          <Card
-            style={{
-              width: 500,
-              margin: `2rem 2rem`,
-            }}
-            title="Company Name"
-            bordered={false}
-            cover={
-              <img
-                alt="example"
-                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-              />
+          }
+          actions={[
+            <DollarCircleOutlined key="setting" />,
+            <InfoCircleOutlined key="edit" />,
+            <EnvironmentOutlined key="ellipsis" />,
+          ]}
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
             }
-            actions={[
-              <DollarCircleOutlined key="setting" />,
-              <InfoCircleOutlined key="edit" />,
-              <EnvironmentOutlined key="ellipsis" />,
-            ]}
-          >
-            <Meta
-              avatar={
-                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-              }
-              title="Photographer Name"
-              description="This is the description"
+            title="Photographer Name"
+            description="This is the description"
+          />
+        </Card>
+        <Card
+          style={{
+            width: 500,
+            margin: `2rem 2rem`,
+          }}
+          title="Company Name"
+          bordered={false}
+          cover={
+            <img
+              alt="example"
+              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
             />
-          </Card>
-        </div>
+          }
+          actions={[
+            <DollarCircleOutlined key="setting" />,
+            <InfoCircleOutlined key="edit" />,
+            <EnvironmentOutlined key="ellipsis" />,
+          ]}
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+            }
+            title="Photographer Name"
+            description="This is the description"
+          />
+        </Card>
       </div>
     </div>
   );
 };
 
 const containerStyle = {
-  width: "100%",
-};
-
-const boxStyle = {
   boxSizing: "border-box",
   backgroundColor: "#FCF7EF",
   height: "auto",
-  width: "auto",
+  width: "100%",
   display: "flex",
   boxShadow: "6px 6px 0px #CBC8C8",
   justifyContent: "center",
